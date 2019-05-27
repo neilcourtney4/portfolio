8d5b965a3c4fdfd7e4e58157d728ac0b379bc577
neilcourtney4 <neil.courtney4@gmail.com>
2019-05-27 13:59:07 +0100

    Add files via upload

---
+++ website/webdeveloper/js/plugin/custom2.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+
+    
+  jQuery(window).load(function() {
+    jQuery("#preloader").delay(100).fadeOut("slow");
+    jQuery("#load").delay(100).fadeOut("slow");
+  });
+
+    /*---------------------------------------------*
+     * Scroll Up Section
+     ---------------------------------------------*/
+
+    $(function () {
+  $('.example-popover').popover({
+    container: 'body'
+  })
+})
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+    /*---------------------------------------------*
+     * Scroll Total Navbar
+     ---------------------------------------------*/
+
+    
+   
+});
